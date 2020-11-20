f9350ab4999efc155c64d252110b69d8c6d7c571
Ritwik Mukherjee <mritwik369@gmail.com>
2020-11-20 13:35:29 +0530

    map dark mode

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let lat = new URL(window.location.href).searchParams.get("lat");
 let lng = new URL(window.location.href).searchParams.get("lng");
 
 var mymap = L.map('mapid').setView([lat, lng], 12);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 19,
 }).addTo(mymap);
 
